da12ff52c7aeafc63deca0b645b2d4dfbe40aba4
sksinghdev <66466025+sksinghdev@users.noreply.github.com>
2025-06-12 17:54:16 +0530

    fix: "app router fixes"

--- features/authentication/lib/core/injections/auth_router.dart
+++ features/authentication/lib/core/injections/auth_router.dart
@@ -1,32 +1,11 @@
-// import 'package:auto_route/auto_route.dart';
 
-// import '../../presentation/page/login_page.dart';
-
-// part 'auth_router.gr.dart';
- 
-
-// @AutoRouterConfig()
-// class AuthRouter extends _$AuthRouter {
-//   @override
-//   final List<AutoRoute> routes = [
-//     AutoRoute(page: LoginRoute.page),
-    
-//   ];
-// }
-
-// authentication/core/injections/auth_router.dart
+import 'package:authentication/core/injections/auth_router.gr.dart';
 import 'package:auto_route/auto_route.dart';
-import '../../presentation/page/login_page.dart'; // Ensure correct relative path
-import 'package:ecommerce_app/src/application/router/app_router.dart';
 
-// Remove: part 'auth_router.gr.dart'; // No longer needed
-// Remove: @AutoRouterConfig()
-// Remove: extends _$AuthRouter
 
-class AuthRouter { // This is now a simple class that provides a list of routes
+@AutoRouterConfig()
+class AuthRouter extends RootStackRouter{  
   final List<AutoRoute> routes = [
-    AutoRoute(page: LoginRoute.page, path: '/login'), // Added a path for clarity
-    // You can add more authentication routes here, e.g.,
-    // AutoRoute(page: RegisterRoute.page, path: '/register'),
+    AutoRoute(page: LoginRoute.page, path: '/login'),  
   ];
 }
--- lib/src/application/router/app_router.dart
+++ lib/src/application/router/app_router.dart
@@ -2,12 +2,11 @@ import 'package:auto_route/auto_route.dart';
 import 'package:authentication/core/injections/auth_router.dart';
 
 import '../../presentation/widgets/splash_screen.dart';
-import 'package:authentication/presentation/page/login_page.dart';
 part 'app_router.gr.dart';
  
 
 @AutoRouterConfig()
-class AppRouter extends _$AppRouter {
+class AppRouter extends RootStackRouter{
   @override
   final List<AutoRoute> routes = [
     
