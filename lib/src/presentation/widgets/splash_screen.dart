9b349600de33bf1411b32fc7ad3925f02f94db33
sksinghdev <66466025+sksinghdev@users.noreply.github.com>
2025-06-12 17:58:50 +0530

    fix: "navigation fixes in splash screen"

--- lib/src/presentation/widgets/splash_screen.dart
+++ lib/src/presentation/widgets/splash_screen.dart
@@ -1,8 +1,8 @@
+import 'package:authentication/core/injections/auth_router.gr.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:common/presentation/bloc/network_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:common/common.dart';
-import 'package:ecommerce_app/src/application/router/app_router.dart';
 import '../../application/di/injection_container.dart';
 
 @RoutePage()
@@ -13,24 +13,27 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider.value(
       value: injector<ConnectivityCubit>(),
-      child: BlocBuilder<ConnectivityCubit, NetworkStatus>(
-        builder: (context, state) {
+      child: BlocConsumer<ConnectivityCubit, NetworkStatus>(
+        listener: (context, state) {
           if (state == NetworkStatus.connected) {
             Future.delayed(const Duration(seconds: 2), () {
               WidgetsBinding.instance.addPostFrameCallback((_) {
-                context.router.replace(const LoginRoute());
+                context.replaceRoute(const LoginRoute());
               });
             });
           }
 
-          return Scaffold(
+         
+        },
+        builder: (context, state){
+           return Scaffold(
             body: Center(
               child: state == NetworkStatus.disconnected
                   ? const Text("No Internet Connection")
                   : const CircularProgressIndicator(),
             ),
           );
-        },
+        }
       ),
     );
   }
