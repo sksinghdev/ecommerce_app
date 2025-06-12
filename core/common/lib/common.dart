d2e7d55babc944bb1ceacaba7329038d34aea1ca
sksinghdev <66466025+sksinghdev@users.noreply.github.com>
2025-06-12 17:56:47 +0530

    fix: "other imports added in commons "

--- core/common/lib/common.dart
+++ core/common/lib/common.dart
@@ -5,7 +5,7 @@ export 'package:firebase_auth/firebase_auth.dart';
 export 'package:google_sign_in/google_sign_in.dart'; 
 export  'package:flutter_bloc/flutter_bloc.dart';
 export 'package:connectivity_plus/connectivity_plus.dart';
-export 'package:google_fonts/google_fonts.dart';
+export 'package:firebase_core/firebase_core.dart';
 
 
  
