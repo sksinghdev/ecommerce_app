7a3cc3eed4c3dcb1b87f7cd81544eee6aff4109c
sksinghdev <66466025+sksinghdev@users.noreply.github.com>
2025-06-12 17:57:26 +0530

    fix: "minor routes fixes"

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:ecommerce_app/src/application/di/service_locator.dart';
 import 'package:ecommerce_app/src/application/router/app_router.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+ import 'package:common/common.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'src/application/di/injection_container.dart' as di;
 final _appRouter =  AppRouter();
 void main() async{
-   WidgetsFlutterBinding.ensureInitialized();
-  
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   await _registerDependencies();
   setupLocator();
   runApp(const MyApp());
@@ -22,17 +23,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp.router(routerConfig: _appRouter.config(),
-   
-            debugShowCheckedModeBanner: false,
-            title: 'E-Commerce App',
-            theme: ThemeData(
-              primarySwatch: Colors.blue,
-              visualDensity: VisualDensity.adaptivePlatformDensity,
-            ),
-    
-    
-    );
+   try {
+      return MaterialApp.router(
+        routerConfig: _appRouter.config(),
+        debugShowCheckedModeBanner: false,
+        title: 'E-Commerce App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+      );
+    } catch (e, stack) {
+      debugPrint('Error during router initialization: $e\n$stack');
+      return const MaterialApp(home: Scaffold(body: Center(child: Text('App initialization failed'))));
+    }
   }
 }
 
