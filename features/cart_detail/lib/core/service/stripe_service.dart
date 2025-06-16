b28ce6f28eedc586f7ea5cbe85d365a503321c6e
sksinghdev <66466025+sksinghdev@users.noreply.github.com>
2025-06-16 08:05:38 +0530

    fix: "created seprate class for payment strip service "

---
+++ features/cart_detail/lib/core/service/stripe_service.dart
@@ -0,0 +1,21 @@
+// stripe_service.dart
+import 'package:flutter_stripe/flutter_stripe.dart';
+
+class StripeService {
+  final Stripe stripe;
+
+   StripeService({Stripe? stripeInstance}) : stripe = stripeInstance ?? Stripe.instance;
+
+  Future<void> initPaymentSheet(String clientSecret) async {
+    await stripe.initPaymentSheet(
+      paymentSheetParameters: SetupPaymentSheetParameters(
+        paymentIntentClientSecret: clientSecret,
+        merchantDisplayName: 'FakeStore',
+      ),
+    );
+  }
+
+  Future<void> presentPaymentSheet() async {
+    await stripe.presentPaymentSheet();
+  }
+}
